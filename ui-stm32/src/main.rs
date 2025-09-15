b623a789415023a9a01ba5c0fd07be617dde0584
Richard Barnes <rlb@ipv.sx>
2025-09-15 09:23:15 -1000

    First stab at sound

--- ui-stm32/src/main.rs
+++ ui-stm32/src/main.rs
@@ -9,7 +9,11 @@ use ui_app::{App, Event};
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{i2c::Error, mode::Async, usart::UartRx};
+use embassy_stm32::{
+    i2c::{mode::Master, Error, I2c},
+    mode::{Async, Blocking},
+    usart::UartRx,
+};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::{Channel, Sender};
 use embassy_time::Timer;
@@ -184,6 +188,12 @@ async fn main(spawner: Spawner) {
     };
     let p = embassy_stm32::init(config);
 
+    // Do audio chip setup over I2C
+    let i2c = embassy_stm32::i2c::I2c::new_blocking(p.I2C1, p.PB6, p.PB7, Default::default());
+    let mut audio_control = AudioControl::new(i2c);
+    audio_control.init();
+
+    // Set up I2S
     let mut wavetable = [0u16; 1200];
     for (i, frame) in wavetable.chunks_mut(2).enumerate() {
         frame[0] = ((((i / 150) % 2) * 2048) as i16 - 1024) as u16; // 160 Hz square wave L
@@ -218,5 +228,121 @@ async fn main(spawner: Spawner) {
     info!("playing...");
     loop {
         i2s.write(&wavetable).await.ok();
+    }
+}
+
+struct AudioControl {
+    i2c: I2c<'static, Blocking, Master>,
+    r: [u16; 128],
+}
+
+impl AudioControl {
+    const VALUE_MASK: u16 = 0x1ff;
+
+    fn new(i2c: I2c<'static, Blocking, Master>) -> Self {
+        Self { i2c, r: [0; 128] }
+    }
+
+    async fn init(&mut self) {
+        // Reset the wm8960
+        self.set_register(0x0F, 0b1_0000_0000);
+        Timer::after_millis(100).await;
+
+        // Set the power
+        self.set_register(0x19, 0b0_1111_1110);
+
+        // Enable outputs
+        self.set_register(0x1A, 0b1_1110_0001);
+
+        // Enable lr mixer ctrl
+        // self.set_register(0x2F, 0b0_0000_0000);
+        self.set_register(0x2F, 0b0_0010_1100);
+
+        // Disable soft mute and ADC high pass filter
+        self.set_register(0x05, 0b0_0000_0000);
+
+        // Set clocks for 8kHz
+        self.set_register(0x34, 0b0_0000_1000);
+        self.set_register(0x35, 0b0_0011_0001);
+        self.set_register(0x36, 0b0_0010_0110);
+        self.set_register(0x37, 0b0_1110_1001);
+        self.set_register(0x04, 0b1_1011_0001);
+        self.set_register(0x08, 0b1_1100_1100);
+        self.set_register(0x1B, 0b0_0000_0101);
+
+        // Set mono
+        self.set_bit(0x17, 4, true);
+        self.set_bit(0x2A, 6, false);
+
+        // Set volumes
+        const DEFAULT_VOLUME: u16 = 0b110_0111;
+        const DEFAULT_MIC_VOLUME: u16 = 0b11_1111;
+        self.set_bits(0x00, 0b1_0011_1111, 0x100 + DEFAULT_MIC_VOLUME);
+        self.set_bits(0x02, 0b1_0111_1111, DEFAULT_VOLUME);
+        self.set_bits(0x03, 0b1_0111_1111, 0x100 + DEFAULT_VOLUME);
+
+        // Enable the outputs
+        self.set_register(0x31, 0b0_0111_0111);
+
+        // Set DAC left and right volumes
+        self.set_register(0x0A, 0b1_1111_1111);
+        self.set_register(0x0B, 0b1_1111_1111);
+
+        // Set left and right mixer
+        self.set_register(0x22, 0b1_0000_0000);
+        self.set_register(0x25, 0b1_0000_0000);
+
+        self.set_bits(0x2B, 0b0_0111_0000, 0b0_0111_000);
+
+        // Enable DAC softmute
+        self.set_bit(0x06, 3, true);
+
+        // Set the Master mode (1), I2S to 16 bit words
+        // Set audio data format to i2s mode
+        self.set_register(0x07, 0b0_0100_0010);
+
+        // Unmute the mic
+        self.set_bit(0x20, 6, false);
+        self.set_bit(0x20, 8, false);
+        self.set_bit(0x19, 5, true);
+        self.set_bit(0x2f, 5, true);
+        self.set_bit(0x20, 3, true);
+        self.set_bit(0x20, 7, false);
+        self.set_bit(0x20, 6, true);
+        self.set_bit(0x20, 8, true);
+        self.set_bits(0x00, 0b1_1000_0000, 0b1_0000_0000);
+        self.set_bits(0x2B, 0b0_0000_1110, 0b0_0000_1010);
+        self.set_bit(0x19, 1, true);
+    }
+
+    fn set_register(&mut self, addr: u8, value: u16) {
+        self.r[addr as usize] = value & Self::VALUE_MASK;
+        self.write_register(addr);
+    }
+
+    fn set_bit(&mut self, addr: u8, which: usize, value: bool) {
+        defmt::assert!(which < 9);
+        let mask = 1 << which;
+        let value: u16 = value.into();
+        self.r[addr as usize] = (self.r[addr as usize] & !mask) | (value << which);
+        self.write_register(addr);
+    }
+
+    fn set_bits(&mut self, addr: u8, mask: u16, value: u16) {
+        defmt::assert_eq!(mask & !Self::VALUE_MASK, 0);
+        defmt::assert_eq!(!mask & value, 0);
+        self.r[addr as usize] = (self.r[addr as usize] & !mask) | (mask & value);
+        self.write_register(addr);
+    }
+
+    fn write_register(&mut self, addr: u8) {
+        const ADDR_MASK: u16 = 0x7f;
+        const VALUE_MASK: u16 = 0x1ff;
+        const DEVICE_ADDR: u8 = 0x1a;
+
+        let to_write = (((addr as u16) & ADDR_MASK) << 9) | (self.r[addr as usize] & VALUE_MASK);
+        self.i2c
+            .blocking_write(DEVICE_ADDR, &to_write.to_be_bytes())
+            .unwrap();
     }
 }
